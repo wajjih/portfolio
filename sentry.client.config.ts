edb42f56210ad167aeb5531f3da59b87cd83b4b8
wajjih <wajjih22@gmail.com>
2025-04-29 00:23:52 -0400

    Lets see man

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408",
+  dsn: "https://f43f21ea22facce5e9b6e6db40c0f0dd@o4509175230103552.ingest.us.sentry.io/4509175232069632",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
